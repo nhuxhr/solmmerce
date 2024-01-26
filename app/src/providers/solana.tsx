ed53e8a2a2eaf01711951bb50d9e769a83d561d0
Rapheal (jsxclan) <nhuxhr@gmail.com>
2024-01-26 04:04:36 +0100

    feat: impl Solana wallet provider ✨

---
+++ app/src/providers/solana.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { FC, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const SolanaWalletProvider: FC<Props> = ({ children }) => {
+  const endpoint = useMemo(() => process.env.NEXT_PUBLIC_CLUSTER_URL!, []);
+
+  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
